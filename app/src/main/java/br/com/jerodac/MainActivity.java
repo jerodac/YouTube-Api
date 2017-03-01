c62fe3d9cb13e1e28d20d9371776e8aab848ff00
Jean Rodrigo D. Cunha <cin_jcunha@uolinc.com>
2017-03-01 19:54:30 -0300

    Test api youtube, integration

--- app/src/main/java/br/com/jerodac/MainActivity.java
+++ app/src/main/java/br/com/jerodac/MainActivity.java
@@ -3,11 +3,28 @@ package br.com.jerodac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import br.com.jerodac.business.FactoryAsyncTask;
+import br.com.jerodac.business.RestClient;
+import br.com.jerodac.dto.ResponseDto;
+import br.com.jerodac.utils.AppLog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new FactoryAsyncTask<ResponseDto>() {
+            @Override
+            protected ResponseDto doIt() {
+                return RestClient.getAllPlayList();
+            }
+
+            @Override
+            protected void onSuccess(ResponseDto responseDto) {
+                AppLog.v(AppLog.TAG, "success!");
+            }
+        }.execute();
     }
 }
--- app/src/main/java/br/com/jerodac/business/RestClient.java
+++ app/src/main/java/br/com/jerodac/business/RestClient.java
@@ -7,22 +7,22 @@ import br.com.jerodac.dto.ResponseDto;
  */
 public class RestClient {
 
-    String USER = "androiddevelopers";
-    String ID_USER = "UCVHFbqXqoYvEWM1Ddxl0QDg";
-    String KEY_GOOGLE = "AIzaSyBPlVYsLeKqj9u7wjIQFzamljTaFKPVFSE";
+    private static String USER = "androiddevelopers";
+    private static String ID_USER = "UCVHFbqXqoYvEWM1Ddxl0QDg";
+    private static String KEY_GOOGLE = "AIzaSyBPlVYsLeKqj9u7wjIQFzamljTaFKPVFSE";
 
     //Returns playlist of user videos
-    public ResponseDto getAllPlayList() {
+    public static ResponseDto getAllPlayList() {
         return Configuration.getApi().playList(ID_USER, KEY_GOOGLE);
     }
 
     //Returns videos from a specific playlist
-    public ResponseDto getVideosByPLayList(String playListId) {
+    public static ResponseDto getVideosByPLayList(String playListId) {
         return Configuration.getApi().playListItem(playListId, KEY_GOOGLE);
     }
 
     //Returns information about a video
-    public ResponseDto getInfoByVideo(String videoId) {
+    public static ResponseDto getInfoByVideo(String videoId) {
         return Configuration.getApi().videoDescribe(videoId, KEY_GOOGLE);
     }
 }
